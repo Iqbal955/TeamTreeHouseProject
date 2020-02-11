a0efe56be5eded3394feb02abc0740bedee79338
Iqbal955 <58709666+Iqbal955@users.noreply.github.com>
2020-02-11 01:10:54 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,116 +1,49 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
+const list = document.querySelectorAll(".student-item");
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+const page = document.querySelectorAll(".page");
 
-
-//Quotes array
-
-quotes = [ {
-  quote: "Love For All, Hatred For None",
-  source: "www.codeofliving.com",
-  citation: "Khalifatul Masih III",
-  year: "1900"
-},
-          {
- 
-          
-  quote: "Never regret anything that made you smile.",
-  source: "www.codeofliving.com",
-  citation: "Mark Twain"
-},
-          
-                {
- 
-          
-  quote: "If I’m gonna tell a real story, I’m gonna start with my name",
-  source: "Kendrick Lamar",
+const showPage = (list, page) => {
   
-},
-          
-          {
-  quote: "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it",
-  source: "Twitter",
-  citation: "Patrick McKenzie",
-  year: "2001"
+ const startIndex = (page * 9) - 9;
+ const endIndex = page * 9;
+  
+ for(i=0;i<list.length; i++) {
+    
+    if(i >= 0 && i <= 10) {
+      
+      list[i].style.display = "";
+      
+    }
    
-          },
-          
-          {
-            quote: "Sometimes later becomes never. Do it now.",
-            source: "Success.com",
-            citation: "N/A",
-            year: "N/A"
-          }
-          
-          
-          
-          
-         ]
-
-
-
-
-
-function getRandomQuote() {
+    else(i >= startIndex && i <= endIndex) {
+      
+      list[i].style.display = "none";
+      
+    }
+  }
   
-  RandomQuote = Math.floor(Math.random() * quotes.length )
-  return quotes[RandomQuote]
-}
-/***
- * `getRandomQuote` function.
- Generating the random quote, by choosing a random number according to the length of quotes array and using the floor function to make it an even number, then returning the quotes array and the one to be generated is the one generated in RandomQuote
-***/
-
-
-
-function printQuote() {
-
-
-var randomQuote = getRandomQuote()
-//the getRandomQuote function previously created is called
-var HTML = ''
-//an empty HTML string is created
-
-
-HTML += '<p class="quote">' + [randomQuote.quote] + '</p>'
-HTML += '<p class="source">' + [randomQuote.source]
-// the HTML variable is concatinated with the quote html and source html, and its concatinated with the randomQuote quote and source
-
-if(randomQuote.citation) {
-HTML += '<span class= "citation">' + [randomQuote.citation] + '</span>'
-  //if the quotes.citation is found, then do as previous just with citation
-}
-  
-  if (randomQuote.year) 
-  {
- HTML += '<span class="year">' + [randomQuote.year] + '</span> </p>'
- }
-document.getElementById('quote-box').innerHTML = HTML;
-  
- //if the quotes.year is found, then do as previous just with citation
-
+ 
+  //How would I call to test the function?
   
 }
-
-/***
- * `printQuote` function
-***/
+showPage(list, 1)
 
 
 
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
-console.log(quotes);
-console.log(printQuote());
 
+*/
+const appendPageLinks = (links) => {
+  
+  const div = document.createElement(div);
+  div.setAttribute("class", "“pagination”");
+  div.appendChild(page);
+  
+  
+  const ul = document.createElement(ul);
+  
+  ul.appendChild(div);
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
-
-//when the load quote button is clicked, the printQuote function is executed
+  
+  
+}
+/*
